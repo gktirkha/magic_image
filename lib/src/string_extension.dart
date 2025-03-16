83a7c911b2cfb2cbc8c2305ed6bd936c7b27ce53
gautam <gautam@gautam.com>
2025-03-16 07:58:41 +0530

    url handling

--- lib/src/string_extension.dart
+++ lib/src/string_extension.dart
@@ -1,12 +1,12 @@
 extension StringExtension on String {
-  bool get isURL => StringUtil.isURL(this);
+  bool get isURL => StringUtil.isURL(Uri.parse(this).toString());
   bool get isSVG => endsWith('.svg');
 }
 
 class StringUtil {
   static bool isURL(String s) => hasMatch(
         s,
-        r"^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,6}(\:[0-9]{1,5})*(/($|[a-zA-Z0-9\.\,\;\?\'\\\+&amp;%\$#\=~_\-]+))*$",
+        r"^((https?|ftps?)://)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,15}(:\d{1,5})?(\/[^\s]*)?$",
       );
 
   static bool hasMatch(String? value, String pattern) {
